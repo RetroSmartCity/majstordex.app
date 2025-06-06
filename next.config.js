8fcb2c0d1c6bed50c74935528d87f47d1c815a30
RetroSmartCity <retrovrsac@gmail.com>
2025-06-06 11:18:41 +0200

    Petak4

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
-const path = require('path');
-
-module.exports = {
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname);
-    return config;
+const nextConfig = {
+  swcMinify: true,
+  transpilePackages: [],
+  experimental: {
+    legacyBrowsers: false, // <== OVDE JE KLJUČ
   },
 };
+
+module.exports = nextConfig;
