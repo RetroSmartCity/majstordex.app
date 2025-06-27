d577d3041741104ca666cccc3b766c9867afad63
RetroSmartCity <retrovrsac@gmail.com>
2025-06-27 17:16:36 +0200

    Ispravljen global.d.ts i dodata validna podrška za GA4

--- global.d.ts
+++ global.d.ts
@@ -1,4 +1,8 @@
-//global.d.ts
-interface Window {
-  gtag?: (...args: any[]) => void;
+// global.d.ts
+export {}; // <- ovo je neophodno da TypeScript ne baci grešku o redefiniciji
+
+declare global {
+  interface Window {
+    gtag?: (...args: any[]) => void;
+  }
 }
