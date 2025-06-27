a6f2f39bda84b598a45f13c9e034e976b01a951d
RetroSmartCity <retrovrsac@gmail.com>
2025-06-27 19:51:01 +0200

    No5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,28 +1,31 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 
+const GA_ID = 'G-JF0XYKPFKP';
+
 export default function Document() {
   return (
     <Html lang="sr">
       <Head>
-        <meta charSet="UTF-8" />
-        <meta name="theme-color" content="#facc15" />
+        {/* Google Analytics GA4 */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_ID}');
+            `,
+          }}
+        />
 
-        {/* Favicons */}
+        {/* Manifest i ikone */}
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-
-        {/* Ako koristiš Google Fonts, dodaj ovo: */}
-        {/*
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
-          rel="stylesheet"
-        />
-        */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="theme-color" content="#facc15" />
       </Head>
       <body>
         <Main />
