c397309ba8c3211c6082812255c10cb945be2f11
RetroSmartCity <retrovrsac@gmail.com>
2026-01-06 17:18:52 +0100

    Add Google Tag Manager (GTM-WTHBXKHS) to _document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,44 +21,19 @@ export default function Document() {
           areaServed: "RS",
           availableLanguage: ["sr"],
         },
-        address: {
-          "@type": "PostalAddress",
-          addressLocality: "Beograd",
-          addressCountry: "RS",
-        },
       },
       {
         "@type": "LocalBusiness",
         "@id": `${siteUrl}#localbusiness`,
         name: "MajstorDex – Električar Beograd 0–24",
-        image: logoUrl,
         telephone: "+381600500063",
         address: {
           "@type": "PostalAddress",
           addressLocality: "Beograd",
           addressCountry: "RS",
         },
-        areaServed: {
-          "@type": "City",
-          name: "Beograd",
-        },
         openingHours: "Mo-Su 00:00-24:00",
         url: siteUrl,
-        priceRange: "$$",
-      },
-      {
-        "@type": "WebSite",
-        "@id": `${siteUrl}#website`,
-        url: siteUrl,
-        name: "MajstorDex",
-        publisher: {
-          "@id": `${siteUrl}#organization`,
-        },
-        potentialAction: {
-          "@type": "SearchAction",
-          target: `${siteUrl}/pretraga?q={search_term_string}`,
-          "query-input": "required name=search_term_string",
-        },
       },
     ],
   };
@@ -66,15 +41,27 @@ export default function Document() {
   return (
     <Html lang="sr">
       <Head>
-        {/* ✅ PWA + Ikone */}
+        {/* 🔥 GOOGLE TAG MANAGER */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];
+              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WTHBXKHS');
+            `,
+          }}
+        />
+
+        {/* PWA */}
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <meta name="theme-color" content="#facc15" />
 
-        {/* ✅ SEO Schema */}
+        {/* SEO Schema */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
@@ -82,6 +69,16 @@ export default function Document() {
       </Head>
 
       <body>
+        {/* GTM NOSCRIPT */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WTHBXKHS"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
